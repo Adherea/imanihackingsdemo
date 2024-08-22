f7b09e0e21ce84d3be91d99fecc84a73355f16c1
DityaSismantoro <dityasismantoro@gmail.com>
2024-08-22 13:12:46 +0800

    first commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,22 @@
-"use client";
+import { useState, useRef, useEffect, ChangeEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState, useEffect, useRef, ChangeEvent } from "react";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import logo from "../../../public/images/logo.png";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import { useRouter, usePathname } from "next/navigation";
+import emailjs from "@emailjs/browser";
 
 export default function Navbar() {
   const t = useTranslations("navbar");
   const [navbar, setNavbar] = useState(true);
+  const [contactPopup, setContactPopup] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState("en");
   const router = useRouter();
   const pathname = usePathname();
   const menuRef = useRef<HTMLDivElement | null>(null);
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   const selectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const nextLocale = e.target.value;
@@ -39,6 +41,35 @@ export default function Navbar() {
     setNavbar((prevState) => !prevState);
   };
 
+  const toggleContactPopup = () => {
+    setContactPopup((prevState) => !prevState);
+  };
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      emailjs
+        .sendForm(
+          "service_ywze8t3", // Ganti dengan Service ID
+          "template_a9xuyc4", // Ganti dengan Template ID
+          formRef.current,
+          "Vcei92itv2gfb6GLz" // Ganti dengan User ID
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            alert("Message sent successfully!");
+            toggleContactPopup();
+          },
+          (error) => {
+            console.log(error.text);
+            alert("Failed to send the message, please try again.");
+          }
+        );
+    }
+  };
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= 768) {
@@ -50,12 +81,13 @@ export default function Navbar() {
       if (menuRef.current && !menuRef.current.contains(event.target as Node) && !navbar) {
         setNavbar(true);
       }
+      if (!contactPopup) {
+        setContactPopup(false);
+      }
     };
 
     window.addEventListener("resize", handleResize);
-    if (!navbar) {
-      document.addEventListener("click", handleClickOutside);
-    }
+    document.addEventListener("click", handleClickOutside);
 
     handleResize();
 
@@ -63,15 +95,16 @@ export default function Navbar() {
       window.removeEventListener("resize", handleResize);
       document.removeEventListener("click", handleClickOutside);
     };
-  }, [navbar]);
+  }, [navbar, contactPopup]);
 
   return (
     <nav className="flex items-center text-white justify-between md:px-12 px-5 text-xl bg-gradient-to-l from-[#7DAAB6] via-[#8EB7C4] to-[#82B3C5]">
       <div>
         <Image src={logo} width={140} alt="logo" />
       </div>
-      <div ref={menuRef}>
-        <ul className={`${navbar ? "md:flex gap-4 hidden" : "items-center gap-4 md:flex absolute bg-blue-400 rounded-md py-4 px-6 right-9"}`}>
+
+      <div ref={menuRef} className="relative flex items-center list-none gap-5">
+        <ul className="md:flex gap-4 hidden">
           <li>
             <a href={"#about"} className="group relative custom-link">
               {t("About")}
@@ -100,9 +133,55 @@ export default function Navbar() {
               {t("Members")}
             </a>
           </li>
+          <li className="md:hidden xl:block block">
+            <button onClick={toggleContactPopup} className="group relative custom-link">
+              {t("Contact")}
+            </button>
+          </li>
         </ul>
+
+        {!navbar && (
+          <div className="overlay" onClick={toggleNavbar}>
+            <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+              <FontAwesomeIcon icon={faXmark} width={20} className={`popup-close`} onClick={toggleNavbar} />
+              <ul className="flex flex-col items-center gap-4">
+                <li>
+                  <a href={"#about"} className="group relative custom-link">
+                    {t("About")}
+                  </a>
+                </li>
+                <li>
+                  <a href={"#countries"} className="group relative custom-link">
+                    {t("Countries")}
+                  </a>
+                </li>
+                <li>
+                  <a href={"#services"} className="group relative custom-link">
+                    {t("Services")}
+                  </a>
+                </li>
+                <li>
+                  <a href={"#news"} className="group relative custom-link">
+                    {t("News")}
+                  </a>
+                </li>
+                <li>
+                  <a href={`/${selectedLanguage}/members`} className="group relative custom-link">
+                    {t("Members")}
+                  </a>
+                </li>
+                <li className="md:hidden xl:block block">
+                  <button onClick={toggleContactPopup} className="group relative custom-link">
+                    {t("Contact")}
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="">
+
+      <div>
         <select className="text-[#000080] bg-transparent rounded-md border border-[#000080] py-1 px-2" onChange={selectChange} value={selectedLanguage}>
           <option value="en">English</option>
           <option value="ja">Japanese</option>
@@ -110,8 +189,25 @@ export default function Navbar() {
       </div>
 
       <div className="md:hidden static" onClick={toggleNavbar}>
-        {navbar ? <FontAwesomeIcon icon={faBars} width={30} /> : <FontAwesomeIcon icon={faXmark} width={30} />}
+        <FontAwesomeIcon icon={!navbar ? faXmark : faBars} className={`${!navbar ? "hidden" : "static"} `} width={30} />
       </div>
+
+      {contactPopup && (
+        <div className="overlay" onClick={toggleContactPopup}>
+          <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+            <FontAwesomeIcon icon={faXmark} width={20} className={`popup-close`} onClick={toggleContactPopup} />
+            <h2>Contact Us</h2>
+            <form ref={formRef} onSubmit={sendEmail}>
+              <input type="text" name="to_name" className="form-input" placeholder="Name" required />
+              <input type="email" name="from_name" className="form-input" placeholder="Email" required />
+              <textarea name="message" className="form-input" placeholder="Message" required></textarea>
+              <button type="submit" className="form-submit">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
